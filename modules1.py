15d6f97dff235304ff9b102384fa1c05436e5701
Joseph-Macalinao <59902035+Joseph-Macalinao@users.noreply.github.com>
2021-05-10 00:39:18 -0700

    add

---
+++ modules1.py
@@ -0,0 +1,6 @@
+def name():
+    print('hey, im cool')
+
+def square(num):
+    print(num * num)
+    return num ** 2
---
+++ modules2.py
@@ -0,0 +1,7 @@
+
+#using import keyword will take all functions form other file and will make them into modules able
+#to be called from dot-notation
+import modules1
+
+modules1.square(3)
+
