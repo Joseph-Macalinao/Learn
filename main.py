92176cb3e52bee6995795bf175f3095c99d54533
Joseph-Macalinao <59902035+Joseph-Macalinao@users.noreply.github.com>
2021-05-10 01:08:09 -0700

    add

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-def car_game():
-    print("Welcome to the car game.")
-    decide = True
-    while decide == True:
-        decision = str(input("What would you like to do: \n1. Start car \n2. Stop car \n3. Quit \n"))
-        if decision.lower() == '1':
-            print('The car is now started!')
-        elif decision.lower() == '2':
-            print('The car is now stopped')
-        elif decision.lower() == '3':
-            decide = False
-        else:
-            print('Input not accepted: please enter a 1, 2, or 3')
-    print('Thanks for playing')
-car_game()
+import package_prac.prac
+package_prac.prac.calc_shipping()
--- modules2.py
+++ modules2.py
@@ -2,6 +2,13 @@
 #using import keyword will take all functions form other file and will make them into modules able
 #to be called from dot-notation
 import modules1
+#Or, use form file import func to call, after typing import, hold ctrl and space to see funtion options
+from modules1 import square
 
+#to access a function from a whole file, use file(module) name and then call func after a '.'
 modules1.square(3)
+
+#from the from statement, you can call the function directly
+
+square(2)
 
